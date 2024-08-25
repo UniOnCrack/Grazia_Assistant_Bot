410e28fda249231a55cc5662098abb57075b6fdd
Uni <nithit_su@cmu.ac.th>
2024-08-25 23:37:42 +0700

    Controller Control

--- control/Control/Control.ino
+++ control/Control/Control.ino
@@ -1,10 +1,10 @@
+#include "Servos_Control\Servos_Control.h"
+#include "Motor_Control\Motors_Control.h"
 #include <ps5Controller.h>
 
 unsigned long lastTimeStamp = 0;
 
-
-void notify()
-{
+void notify() {
   char messageString[200];
   sprintf(messageString, "%4d,%4d,%4d,%4d,%3d,%3d,%3d,%3d,%3d,%3d,%3d,%3d,%3d,%3d,%3d,%3d,%3d,%3d,%3d,%3d,%3d,%3d,%3d,%3d",
   ps5.LStickX(),
@@ -32,33 +32,35 @@ void notify()
   ps5.Mic(),
   ps5.Battery());
 
-  //Only needed to print the message properly on serial monitor. Else we dont need it.
-  if (millis() - lastTimeStamp > 50)
-  {
+  // Only needed to print the message properly on serial monitor. Else we don't need it.
+  if (millis() - lastTimeStamp > 50) {
     Serial.println(messageString);
     lastTimeStamp = millis();
   }
 }
 
-void onConnect()
-{
+void onConnect() {
   Serial.println("Connected!.");
 }
 
-void onDisConnect()
-{
+void onDisConnect() {
   Serial.println("Disconnected!.");    
 }
 
-
 void setup() {
   Serial.begin(115200);
+  
+  // Initialize servos and motors
+  initServos();
+  initMotors();
+
+  // Setup PS5 controller
   ps5.attach(notify);
   ps5.attachOnConnect(onConnect);
   ps5.attachOnDisconnect(onDisConnect);
-  ps5.begin("10:18:49:7d:6d:f4");  //your PS5 controller mac address 
-  while (ps5.isConnected() == false) 
-  { 
+  ps5.begin("10:18:49:7d:6d:f4");  // Replace with your PS5 controller MAC address
+  
+  while (ps5.isConnected() == false) { 
     Serial.println("PS5 controller not found");
     delay(300);
   }
@@ -66,31 +68,53 @@ void setup() {
 }
 
 void loop() {
-  // Check if PS5 controller is connected
   if (ps5.isConnected()) {
-    // Detect button presses and print them to the Serial Monitor
-    if (ps5.PSButton()) {
-      Serial.println("PS Button Pressed");
+    // Control Servos with Analog Sticks
+    int servo1Angle = map(ps5.LStickY(), -128, 127, 0, 180); // Map left stick Y to Servo 1 angle
+    int servo2Angle = map(ps5.RStickY(), -128, 127, 0, 180); // Map right stick Y to Servo 2 angle
+    
+    uint16_t pulse1 = SERVOMIN + ((SERVOMAX - SERVOMIN) * servo1Angle / 180);
+    uint16_t pulse2 = SERVOMIN + ((SERVOMAX - SERVOMIN) * servo2Angle / 180);
+
+    pwm.setPWM(SERVO1_CHANNEL, 0, pulse1);  // Move Servo 1 to angle
+    pwm.setPWM(SERVO2_CHANNEL, 0, pulse2);  // Move Servo 2 to angle
+
+    // Control Motors with D-pad
+    if (ps5.UpLeft()) {
+      moveMotorsForward();  // Move motors forward
+      Serial.println("Motors Moving Forward");
+    } else if (ps5.UpRight()) {
+      moveMotorsBackward();  // Move motors backward
+      Serial.println("Motors Moving Backward");
+    } else {
+      // Stop motors when no direction is pressed
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, LOW);
+      digitalWrite(IN3, LOW);
+      digitalWrite(IN4, LOW);
+      Serial.println("Motors Stopped");
+    }
+
+    // Control specific motor directions with buttons
+    if (ps5.Cross()) {
+      digitalWrite(IN1, HIGH);
+      digitalWrite(IN2, LOW);
+      Serial.println("Motor A Forward");
     }
     if (ps5.Circle()) {
-      Serial.println("Circle Button Pressed");
+      digitalWrite(IN1, LOW);
+      digitalWrite(IN2, HIGH);
+      Serial.println("Motor A Backward");
     }
     if (ps5.Square()) {
-      Serial.println("Square Button Pressed");
+      digitalWrite(IN3, HIGH);
+      digitalWrite(IN4, LOW);
+      Serial.println("Motor B Forward");
     }
     if (ps5.Triangle()) {
-      Serial.println("Triangle Button Pressed");
-    }
-    if (ps5.Cross()) {
-      Serial.println("Cross Button Pressed");
-    }
-    
-    // Detect D-pad button presses
-    if (ps5.UpLeft()) {
-      Serial.println("D-pad Left Pressed");
-    }
-    if (ps5.UpRight()) {
-      Serial.println("D-pad Right Pressed");
+      digitalWrite(IN3, LOW);
+      digitalWrite(IN4, HIGH);
+      Serial.println("Motor B Backward");
     }
   }
   
